41a730cd419c7bd3044d84a077a4e44e35aaf5e7
winston <winstonwork@163.com>
2020-06-14 16:57:33 +0800

    初始化提交

---
+++ src/main/java/com/winston/Hello.java
@@ -0,0 +1,11 @@
+package com.winston;
+
+/**
+ * @author winston
+ * @date 2020/6/14 - 13:59
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
